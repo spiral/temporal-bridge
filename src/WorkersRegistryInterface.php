c4ab59c02c17c0c330b66c97b3a536a5bbc45038
butschster <butschster@gmail.com>
2022-07-18 12:31:58 +0400

    Fixes readme

--- src/WorkersRegistryInterface.php
+++ src/WorkersRegistryInterface.php
@@ -11,18 +11,18 @@ use Temporal\Worker\WorkerOptions;
 interface WorkersRegistryInterface
 {
     /**
-     * Register a new temporal worker with given options
+     * Register a new temporal worker with given task queue and options
      * @throws WorkersRegistryException
      */
     public function register(string $name, ?WorkerOptions $options): void;
 
     /**
-     * Get or create worker by name
+     * Get or create worker by task queue name
      */
     public function get(string $name): WorkerInterface;
 
     /**
-     * Check if a worker with given name registered
+     * Check if a worker with given task queue name registered
      */
     public function has(string $name): bool;
 }
