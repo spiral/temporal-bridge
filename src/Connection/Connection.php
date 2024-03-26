8f0044db3a27c800b9a27243382f104d7bd2079a
Pavel Buchnev <butschster@gmail.com>
2024-03-26 11:55:51 +0400

    Update src/Connection/Connection.php
    Co-authored-by: Aleksei Gagarin <roxblnfk@ya.ru>

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -6,6 +6,9 @@ namespace Spiral\TemporalBridge\Connection;
 
 class Connection
 {
+    /**
+     * @param non-empty-string $address
+     */
     public function __construct(
         public readonly string $address,
     ) {
