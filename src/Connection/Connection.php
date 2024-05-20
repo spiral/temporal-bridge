b6f78918a80337bf7e3c549a66fb3cb2df98e4c5
Pavel Buchnev <butschster@gmail.com>
2024-05-20 11:17:30 +0400

    Update src/Connection/Connection.php
    Co-authored-by: Aleksei Gagarin <roxblnfk@ya.ru>

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -8,6 +8,10 @@ class Connection
 {
     /**
      * @param non-empty-string $address
+     *
+     * ```php
+     * new Connection('localhost:7233');
+     * ```
      */
     public function __construct(
         public readonly string $address,
